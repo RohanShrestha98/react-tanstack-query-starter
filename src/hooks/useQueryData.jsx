a6d303e324971ce3baf3bd961958d34da34ce692
RohanShrestha98 <nahorshrestha@gmail.com>
2023-11-02 16:00:00 +0545

    feat: react-tanstack-query-starter

---
+++ src/hooks/useQueryData.jsx
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosPrivate from "./useAxiosPrivate";
+
+export const useQueryData = (key, path, params = "", enabled = true) => {
+  const axiosPrivate = useAxiosPrivate();
+
+  return useQuery({
+    queryKey: [key, params],
+    queryFn: () =>
+      axiosPrivate({
+        url: path,
+        method: "get",
+        params: params,
+      }).then((res) => res?.data && res?.data),
+    enabled,
+  });
+};
